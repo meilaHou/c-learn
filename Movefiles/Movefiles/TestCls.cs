79e7441c5eac195b8ed0e41bf7440069b34e8f82
houxl <houxl@git.com>
2016-04-01 10:15:08 +0800

    分支合并和添加不提交的清单

---
+++ Movefiles/Movefiles/TestCls.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Movefiles
+{
+    class TestCls
+    {
+        public struct Person
+        {
+            public string Name;
+            public int Age;
+            public Person(string name, int age)
+            {
+                Name = name;
+                Age = age;
+            }
+            public override string ToString()
+            {
+                return Name + Age.ToString();
+            }
+        }
+        public void testStruct()
+        {
+
+        }
+    }
+}
